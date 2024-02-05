1e9256ec98de682b14011301803846cc4174148e
Leandro Passos <itsmelepassos@gmail.com>
2024-02-04 22:34:06 -0300

    doc references moved to cpf

--- src/components/site/SearchForm.tsx
+++ src/components/site/SearchForm.tsx
@@ -1,6 +1,6 @@
 "use cilent";
 
-import { escapeDoc } from "@/utils/escapeDoc";
+import { escapeCPF } from "@/utils/escapeCPF";
 import { useState } from "react";
 
 type Props = {
@@ -9,7 +9,7 @@ type Props = {
 };
 
 export const SearchForm = ({ onSearchButton, loading }: Props) => {
-  const [docInput, setDocInput] = useState("");
+  const [cpfInput, setCpfInput] = useState("");
 
   return (
     <div>
@@ -20,13 +20,13 @@ export const SearchForm = ({ onSearchButton, loading }: Props) => {
         placeholder="Digite seu CPF"
         className="w-full p-3 bg-white text-black text-center outline-none rounded-lg disabled:opacity-50"
         autoFocus
-        value={docInput}
-        onChange={event => setDocInput(escapeDoc(event.target.value))}
+        value={cpfInput}
+        onChange={event => setCpfInput(escapeCPF(event.target.value))}
         disabled={loading}
       />
       <button
         className="w-full p-3 mt-3 rounded-lg bg-blue-500 text-white text-4xl border-b-4 border-blue-600 active:border-0 disabled:opacity-50"
-        onClick={() => onSearchButton(docInput)}
+        onClick={() => onSearchButton(cpfInput)}
         disabled={loading}
       >
         {loading ? 'Buscando...' : 'Entrar'}
